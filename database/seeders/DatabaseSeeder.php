5c193034867b2d0693944c75162de70808e69dac
Adrian <benedicto.adrian@ncst.edu.ph>
2026-05-16 22:03:49 +0800

    feat: update DatabaseSeeder to include instructor and student user creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Instructor;
+use App\Models\Student;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +17,36 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Administrator',
             'email' => 'admin@example.com',
+            'password' => '@Admin2221',
             'is_admin' => true,
+        ]);
+
+        $instructor = \App\Models\Instructor::factory()->create([
+            'first_name' => 'Teacher',
+            'last_name' => 'Demo',
+        ]);
+
+        User::factory()->create([
+            'name' => "$instructor->first_name $instructor->last_name",
+            'email' => 'teacher@example.com',
+            'password' => '@Teacher2221',
+            'is_admin' => false,
+            'profileable_id' => $instructor->id,
+            'profileable_type' => Instructor::class,
+        ]);
+
+        $student = Student::factory()->create([
+            'first_name' => 'Student',
+            'last_name' => 'Demo',
+        ]);
+
+        User::factory()->create([
+            'name' => "$student->first_name $student->last_name",
+            'email' => 'student@example.com',
+            'password' => '@Student2221',
+            'is_admin' => false,
+            'profileable_id' => $student->id,
+            'profileable_type' => Student::class,
         ]);
 
         $this->call([
